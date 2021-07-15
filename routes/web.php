cfc91f92b4da4a73b3ee182037c2c164b9d019ef
Marvin Aguilar <aguilar.marvin07@gmail.com>
2021-07-15 08:08:16 -0500

    db

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,10 @@ Route::get('/', 'HomeController@index')->name('home');
 Route::get('registro', 'RegistroController@register');
 Route::get('ver', 'RegistroController@ver');
 Route::post('registrar', 'RegistroController@registrar')->name('registrar');
+
+// DB_CONNECTION=couchdb
+// DB_HOST=45.77.193.128
+// DB_PORT=5984
+// DB_DATABASE=clinica
+// DB_USERNAME=admin
+// DB_PASSWORD=Vpn1234.*
